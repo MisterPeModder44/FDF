d0ba0be7dba5801bf9ca24480e284c3739efd662
Yanis GUAYE <yguaye@student.42.fr>
2017-12-18 01:20:07 +0100

    Add libft as a submodule

--- libft/srcs/base/base.mk
+++
@@ -1,7 +0,0 @@
-MODULES += base
-
--include	srcs/base/character/character.mk \
-			srcs/base/io/io.mk \
-			srcs/base/list/list.mk \
-			srcs/base/memory/memory.mk \
-			srcs/base/string/string.mk \
